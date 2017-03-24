356432f2b064aeff64704dd0601fa20e659b00c0
jbarast <jbarast@gmail.com>
2017-03-24 18:48:44 +0100

    Pedir al usuario que diga el tamaño del tablero y que este se visualice despues. @jbarast

--- src/apuntesClaseZalla2017Jbarast/hundirLaFlota/HundirLaFlota.java
+++ src/apuntesClaseZalla2017Jbarast/hundirLaFlota/HundirLaFlota.java
@@ -4,11 +4,47 @@
 
 package apuntesClaseZalla2017Jbarast.hundirLaFlota;
 
+import java.util.Scanner;
+
 public class HundirLaFlota {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		@SuppressWarnings("resource")
+		Scanner teclado = new Scanner(System.in);
+		int x, y;
+
+		System.out.println("Coordenada x: ");
+		x = teclado.nextInt();
+		System.out.println("Coordenada x: ");
+		y = teclado.nextInt();
+
+		// TODO Hacer el tablero.
+
+		// Creacion del tablero
+
+		char[][] tablero;
+
+		tablero = new char[x][y];
+
+		// Rellenamos primero el tablero con puntos.
+		for (int xArray = 0; xArray < tablero.length; xArray++) {
+			for (int yArray = 0; yArray < tablero[xArray].length; yArray++) {
+				tablero[xArray][yArray] = '.';
+			}
+		}
+
+		// Visualizamos el tablero.
+
+		for (int xArray = 0; xArray < tablero.length; xArray++) {
+			for (int yArray = 0; yArray < tablero[xArray].length; yArray++) {
+
+				System.out.print(tablero[xArray][yArray]);
+			}
+			System.out.println();
+		}
+
+		// ///
 	}
 
 }
