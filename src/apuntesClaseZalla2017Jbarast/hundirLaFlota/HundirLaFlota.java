197079886820622b4236003e1d22ffec2d15ccc5
jbarast <jbarast@gmail.com>
2017-03-24 18:35:54 +0100

    Creacion de la estructura para el ejercicio de clase del 24-03-2017 de hundir de la flota. @jbarast

---
+++ src/apuntesClaseZalla2017Jbarast/hundirLaFlota/HundirLaFlota.java
@@ -0,0 +1,14 @@
+// HundirLaFlota.java
+// Author: J.Barnes
+// 24-03-2017
+
+package apuntesClaseZalla2017Jbarast.hundirLaFlota;
+
+public class HundirLaFlota {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/apuntesClaseZalla2017Jbarast/hundirLaFlota/package-info.java
@@ -0,0 +1,10 @@
+//package-info.java
+/**
+ * 
+ */
+/**
+ * @author J.Barnes Astobiza
+ * @author jbarast
+ *
+ */
+package apuntesClaseZalla2017Jbarast.hundirLaFlota;
