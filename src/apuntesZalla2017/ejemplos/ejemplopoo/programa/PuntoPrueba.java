0407e9edda76804d0fbcf812aeae964b0f7fcb15
jbarast <jbarast@gmail.com>
2017-03-29 17:00:29 +0200

    Constructores metidos en el objeto Punto. Autor @jbarast

--- src/apuntesZalla2017/ejemplos/ejemplopoo/programa/PuntoPrueba.java
+++ src/apuntesZalla2017/ejemplos/ejemplopoo/programa/PuntoPrueba.java
@@ -1,11 +1,12 @@
 //PuntoPrueba.java
 //Autor: jbarast  
 // https://github.com/jbarast
-//Fecha: 29/09/2017
+//Fecha: 29/03/2017
 //Nota: Programa de ejemplo para ver como funciona la llamada a objetos.
 
 package apuntesZalla2017.ejemplos.ejemplopoo.programa;
 
+//Importamos el objeto punto.
 import apuntesZalla2017.ejemplos.ejemplopoo.tipos.Punto;
 
 public class PuntoPrueba {
@@ -16,16 +17,35 @@ public class PuntoPrueba {
 
 		Punto pt1; // Si estan los dos en el mismo paquete, no hace falta
 					// importor el objeto.
+		Punto pt2;
 		pt1 = new Punto(); // new es una llamada al constructor.
 
-		// meter los datos.
-		pt1.x = 5;
-		pt1.y = 7;
+		pt2 = new Punto(43.2345, -4.2345);
 
-		System.out.println(String.format("Punto pt1(%d,%d)", pt1.x, pt1.y));
+		// meter los datos.
+		pt1.setX(5);
+		pt1.setY(7);
+
+		// Meter los datos formato nuevo.
+
+		// pt2.setXDouble(5.1);
+		// pt2.setYDouble(107.8);
+
+		// Miramos como aparecen en consola:
+
+		System.out.println(String.format("Punto pt1(%d,%d)", pt1.getX(), pt1.getY()));
 		// Siendo lo mismo que:
 		// System.out.format("P(%d,%d)\n",pt1.x,pt1.y);
 
+		System.out.println(String.format("Punto pt1(%.2f : %.2f)", pt1.getXDouble(), pt1.getYDouble()));
+
+		System.out.println(String.format("Punto pt2(%d,%d)", pt2.getX(), pt2.getY()));
+		// Siendo lo mismo que:
+		// System.out.format("P(%d,%d)\n",pt1.x,pt1.y);
+
+		System.out.println(String.format("Punto pt2(%.2f : %.2f)", pt2.getXDouble(), pt2.getYDouble()));
+
+		// /
 	}
 
 }
--- src/apuntesZalla2017/ejemplos/ejemplopoo/tipos/Punto.java
+++ src/apuntesZalla2017/ejemplos/ejemplopoo/tipos/Punto.java
@@ -8,7 +8,91 @@ package apuntesZalla2017.ejemplos.ejemplopoo.tipos;
 
 public class Punto {
 
-	// Variables del objeto.
-	public int x;
-	public int y;
+	/*
+	 * ************
+	 * VARIABLES ***********
+	 */
+	private double x; // Los private solo se pueden aceder desde la clase
+						// propia.
+	private double y;
+
+	/*
+	 * ************
+	 * GET Y SET ***********
+	 */
+
+	// Creacion de metodos.
+
+	// Metodos set y ge para x e y. En este caso maneja int. Por que como al
+	// principio el codigo antiguo la x y la y eran int, para que siga
+	// funcionando con los programas antiguas, pueda seguir funcionando.
+
+	public int getX() {
+		return (int) x;
+	}
+
+	public void setX(int x) {
+		this.x = (double) x; // la this.x, es la x de la clase. La x normal es
+								// la x del
+		// metodo, que introduce el usuario.
+	}
+
+	public int getY() {
+		return (int) y;
+	}
+
+	public void setY(int y) {
+		this.y = (double) y;
+	}
+
+	// Metodos set y get, para que maneje doubles.
+
+	public double getXDouble() {
+		return x;
+	}
+
+	public void setXDouble(double x) {
+		this.x = x;
+	}
+
+	public double getYDouble() {
+		return y;
+	}
+
+	public void setYDouble(double y) {
+		this.y = y;
+	}
+
+	/*
+	 * ************
+	 * CONSTRUCTOR ***********
+	 */
+	// Para el caso que son doubles.
+	public Punto(double x, double y) {
+		setXDouble(x);
+		setYDouble(y);
+	}
+
+	// Para el caso que son int.
+	public Punto(int x, int y) {
+		// setX(x);
+		// setY(y);
+		// Delegando.
+
+		this((double) x, (double) y); // Pasan el marron al constructor gordo.
+	}
+
+	// Para el caso que no pongo ninguno. Lo pongo como doubles.
+	public Punto() {
+		// setXDouble(0); // En principio no hace falta ponerlo, coje como
+		// default
+		// el valor de 0,0
+		// setYDouble(0);
+
+		// Otra forma de hacerlo.
+		this(0.0, 0.0); // Delega el constructor, menos codigo.
+
+	}
+
+	// /
 }
