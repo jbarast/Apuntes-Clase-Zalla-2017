d06e75b6277185a963ec32de6df11a10792db1c5
jbarast <jbarast@gmail.com>
2017-04-13 10:25:02 +0200

    Aprendiendo javadoc @jbarast

--- src/apuntesZalla2017/laboratoriosLibroJava7/tipos/Alumno.java
+++ src/apuntesZalla2017/laboratoriosLibroJava7/tipos/Alumno.java
@@ -1,11 +1,18 @@
-/**
- * <code>Alumno</code>
- */
-
 package apuntesZalla2017.laboratoriosLibroJava7.tipos;
 
 import java.util.ArrayList;
 
+/**
+ * <h1>Definicion:</h1>
+ * <p>
+ * La clase <code>Alumno.java</code> define el objeto Alumno.
+ * </p>
+ * 
+ * @author jbarast
+ * @version 13/04/2017
+ * @see https://github.com/jbarast/Apuntes-Clase-Zalla-2017
+ * 
+ */
 public class Alumno {
 
 	// Atributos.
@@ -16,12 +23,21 @@ public class Alumno {
 	ArrayList<Double> notas1 = new ArrayList<Double>();
 
 	// Constructor.
+	/**
+	 * Constructor con todos los elementos.
+	 * 
+	 * @param numMatricula
+	 * @param nombre
+	 */
 	public Alumno(int numMatricula, String nombre) {
 		super();
 		this.numMatricula = numMatricula;
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Constructor vacio. Crea el alumno "Anonimo" con el codigo 0.
+	 */
 	public Alumno() {
 		this(0, "Anonimo");
 	}
@@ -46,13 +62,22 @@ public class Alumno {
 	// Otros metodos.
 	@Override
 	public String toString() {
-		return "Alumno [numMatricula=" + numMatricula + ", nombre=" + nombre + "]";
+		return "Alumno [numMatricula=" + numMatricula + ", nombre=" + nombre
+				+ "]";
 	}
 
 	// Almacenar dos notas en dos ArrayList(double)
 	// in: double nota1 y double nota2
 	// out: --
 	// Notas:--
+
+	/**
+	 * Mete en dos ArrayList<double> las notas1 y notas2
+	 * 
+	 * @param nota1
+	 * @param nota2
+	 *            Notas: --
+	 */
 
 	public void ponNota(double nota1, double nota2) {
 		// TODO Meter las notas en las ArrayList
